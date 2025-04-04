f085cd2033e3d6da94f2555072eb402af7e050f9
sarawoot.golf@hotmail.com <sarawoot.golf@hotmail.com>
2025-04-04 13:02:33 +0700

    add Sonarqube SAST to main.yml

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "ecr" {
   name                 = var.ecr_name
   image_tag_mutability = "MUTABLE"
-
+  
   encryption_configuration {
     encryption_type = "AES256"
   }
