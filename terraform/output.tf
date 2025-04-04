f9cc1b5d00082cb4403a7f38be920190918b01ec
sarawoot.golf@hotmail.com <sarawoot.golf@hotmail.com>
2025-04-04 15:50:58 +0700

    add trivy and test CI/CD

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,6 @@
 output "public_ip" {
   value = aws_instance.vm1.public_ip
 }
+output "elastic_ip" {
+  value = aws_eip.ip1.public_ip
+}
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -12,23 +12,23 @@ resource "aws_ecr_repository" "ecr" {
 
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket        = var.s3_bucket_name
-  force_destroy = true
-}
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket        = var.s3_bucket_name
+#   force_destroy = true
+# }
 
-resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto" {
-  bucket = aws_s3_bucket.terraform_state.bucket
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto" {
+#   bucket = aws_s3_bucket.terraform_state.bucket
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
