06d1289f004149414290dee8604b368f8a38fee7
sarawoot.golf@hotmail.com <sarawoot.golf@hotmail.com>
2025-04-04 17:15:49 +0700

    add ZAP DAST to main.yml

--- src/test_main.py
+++ src/test_main.py
@@ -12,7 +12,7 @@ def test_read_root():
     assert response.status_code == 200
     
     # Assert the returned JSON is correct
-    assert response.json() == {"message": "FastApi Web Test Version 5.0"}
+    assert response.json() == {"message": "FastApi Version 5.0"}
 
 def test_greet():
     # Test the /greet/{name} endpoint with a dynamic name
