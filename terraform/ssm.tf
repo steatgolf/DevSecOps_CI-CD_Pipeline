2e152478f3886a1b0750fa2cfdeb3d91d67cfeca
sarawoot.golf@hotmail.com <sarawoot.golf@hotmail.com>
2025-04-10 13:03:21 +0700

    edit ssm policy

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -27,6 +27,11 @@ resource "aws_iam_role_policy_attachment" "ec2_ecr" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
 }
 
+resource "aws_iam_instance_profile" "ec2_ssm_instance_profile" {
+  name = "ec2-ssm-instance-profile"
+  role = aws_iam_role.ec2_ssm.name
+}
+
 
 # resource "aws_iam_policy" "ec2_ecr" {
 #   name   = "ec2_ecr"
@@ -51,9 +56,3 @@ resource "aws_iam_role_policy_attachment" "ec2_ecr" {
 #   role       = aws_iam_role.ec2_ssm.name
 #   policy_arn = aws_iam_policy.ec2_ecr.arn
 # }
-
-
-resource "aws_iam_instance_profile" "ec2_ssm_instance_profile" {
-  name = "ec2-ssm-instance-profile"
-  role = aws_iam_role.ec2_ssm.name
-}
